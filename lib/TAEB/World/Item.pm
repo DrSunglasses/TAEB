430af6a655d4dd0114e981c43c3384784e10fa36
Shawn M Moore <sartak@gmail.com>
2009-01-31 03:59:53 -0500

    TAEB::World::Item->new_item. crazy

--- lib/TAEB/World/Item.pm
+++ lib/TAEB/World/Item.pm
@@ -7,6 +7,17 @@ has nhi => (
     required => 1,
 );
 
+sub new_item {
+    my $self = shift;
+
+    my $nhi = NetHack::Item->new(@_);
+    my $nhi_subclass = $nhi->meta->name;
+
+    (my $subclass = $nhi_subclass) =~ s/^NetHack/TAEB::World/;
+
+    return $subclass->new(nhi => $nhi);
+}
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
