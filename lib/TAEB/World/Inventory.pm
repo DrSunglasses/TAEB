05000de1003a6d1b03285f6fd3b81f282cf7c3c8
Shawn M Moore <sartak@gmail.com>
2009-01-31 05:09:20 -0500

    Inventory->find which just calls match on each of the items in the inventory

--- lib/TAEB/World/Inventory.pm
+++ lib/TAEB/World/Inventory.pm
@@ -2,6 +2,15 @@ package TAEB::World::Inventory;
 use TAEB::OO;
 extends 'NetHack::Inventory';
 
+sub find {
+    my $self = shift;
+
+    for my $item (sort $self->items) {
+        return $item if $item->match(@_);
+    }
+
+    return;
+}
 
 __PACKAGE__->meta->make_immutable;
 no TAEB::OO;
