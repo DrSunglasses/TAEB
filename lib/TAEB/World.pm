83071ec1db5a196d7c4fa05e4a339fcd1ccf03ab
Shawn M Moore <sartak@gmail.com>
2009-01-31 03:45:21 -0500

    Load NetHack::Item classes

--- lib/TAEB/World.pm
+++ lib/TAEB/World.pm
@@ -4,10 +4,17 @@ use warnings;
 
 use Module::Pluggable (
     require     => 1,
+    sub_name    => 'load_nhi_classes',
+    search_path => ['NetHack::Item'],
+);
+
+use Module::Pluggable (
+    require     => 1,
     sub_name    => 'load_world_classes',
     search_path => ['TAEB::World'],
 );
 
+__PACKAGE__->load_nhi_classes;
 __PACKAGE__->load_world_classes;
 
 1;
