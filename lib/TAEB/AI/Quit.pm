594d86819a9f585ec125c15dc668d7e58b880a87
Shawn M Moore <sartak@gmail.com>
2009-01-31 00:56:14 -0500

    Add the old Human and Quit AIs back in

---
+++ lib/TAEB/AI/Quit.pm
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+package TAEB::AI::Quit;
+use TAEB::OO;
+extends 'TAEB::AI';
+
+=head1 NAME
+
+TAEB::AI::Quit - I just can't take it any more...
+
+=cut
+
+sub next_action { TAEB::Action::Quit->new }
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
