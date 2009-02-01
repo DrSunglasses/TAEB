142af62862c814353515097df2e4db56fc53ea7b
Shawn M Moore <sartak@gmail.com>
2009-02-01 04:05:19 -0500

    Gem hardness and softness

--- lib/TAEB/World/Item/Gem.pm
+++ lib/TAEB/World/Item/Gem.pm
@@ -3,7 +3,8 @@ use TAEB::OO;
 extends 'TAEB::World::Item';
 
 has '+nhi' => (
-    isa => 'NetHack::Item::Gem',
+    isa     => 'NetHack::Item::Gem',
+    handles => [qw/hardness softness/],
 );
 
 __PACKAGE__->meta->make_immutable;
