b6f0a6cb3eb10c02bf5eae7372b8e987d9997adc
Shawn M Moore <sartak@gmail.com>
2009-02-01 04:45:10 -0500

    Wand methods

--- lib/TAEB/World/Item/Wand.pm
+++ lib/TAEB/World/Item/Wand.pm
@@ -4,7 +4,14 @@ extends 'TAEB::World::Item';
 
 has '+nhi' => (
     isa     => 'NetHack::Item::Wand',
-    handles => [qw/recharges_known/],
+    handles => [qw/
+        recharges recharges_known inc_recharges charges charges_known
+        set_charges_unknown subtract_charges charges_spent_this_recharge
+        add_charges_spent_this_recharge reset_charges_spent_this_recharge
+        times_recharged inc_times_recharged spend_charge recharge
+        chance_to_recharge burnt corroded rotted rusty proofed remove_damage
+        maxcharges zaptype
+    /],
 );
 
 __PACKAGE__->meta->make_immutable;
