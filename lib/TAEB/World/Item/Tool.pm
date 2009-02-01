5471dd486b3f3d223059a1b8286732ad2084c632
Shawn M Moore <sartak@gmail.com>
2009-02-01 04:43:50 -0500

    So many methods from Chargeable

--- lib/TAEB/World/Item/Tool.pm
+++ lib/TAEB/World/Item/Tool.pm
@@ -5,8 +5,11 @@ extends 'TAEB::World::Item';
 has '+nhi' => (
     isa     => 'NetHack::Item::Tool',
     handles => [qw/
-        charge charges recharge recharges spend_charge chance_to_recharge burnt
-        corroded rotted rusty proofed remove_damage
+        charge recharges recharges_known inc_recharges charges charges_known
+        set_charges_unknown subtract_charges charges_spent_this_recharge
+        add_charges_spent_this_recharge reset_charges_spent_this_recharge
+        times_recharged inc_times_recharged spend_charge recharge
+        chance_to_recharge burnt corroded rotted rusty proofed remove_damage
     /],
 );
 
