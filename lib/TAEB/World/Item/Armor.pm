3dfad27029017890c856ff5866facdf135724eaf
Shawn M Moore <sartak@gmail.com>
2009-02-01 03:56:09 -0500

    Armor delegation

--- lib/TAEB/World/Item/Armor.pm
+++ lib/TAEB/World/Item/Armor.pm
@@ -4,6 +4,10 @@ extends 'TAEB::World::Item';
 
 has '+nhi' => (
     isa => 'NetHack::Item::Armor',
+    handles => [ qw/
+        ac mc is_worn enchantment numeric_enchantment burnt corroded rotted
+        rusty proofed remove_damage
+    /],
 );
 
 __PACKAGE__->meta->make_immutable;
