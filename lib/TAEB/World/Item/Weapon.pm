5b59cd4be50ee77d2f77332c7bf2ff4dc2b07cd4
Shawn M Moore <sartak@gmail.com>
2009-02-01 04:55:54 -0500

    Last one: weapon

--- lib/TAEB/World/Item/Weapon.pm
+++ lib/TAEB/World/Item/Weapon.pm
@@ -4,6 +4,10 @@ extends 'TAEB::World::Item';
 
 has '+nhi' => (
     isa => 'NetHack::Item::Weapon',
+    handles => [qw/
+        is_poisoned is_worn enchantment numeric_enchantment burnt corroded
+        rotted rusty proofed remove_damage
+    /],
 );
 
 __PACKAGE__->meta->make_immutable;
