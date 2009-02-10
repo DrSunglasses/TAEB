3276eac60c9a1d9f5d9fe5ec06ab32ce39f164cd
doy <doy@tozt.net>
2009-02-09 23:18:06 -0500

    error message should point to TAEBDIR, not be hardcoded to ~/.taeb

--- lib/TAEB/Config.pm
+++ lib/TAEB/Config.pm
@@ -33,7 +33,7 @@ has file => (
         -e $_ and return $_ for @locations;
 
         $SIG{__DIE__} = 'DEFAULT';
-        die "Could not find a config file. You should copy TAEB's etc/config.yml into ~/.taeb/config.yml!";
+        die "Could not find a config file. You should copy TAEB's etc/config.yml into $ENV{TAEBDIR}/config.yml!";
     },
 );
 
