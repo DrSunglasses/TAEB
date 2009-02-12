e85591aaa1b7028fbdfa7be151a8f9d1f97c9c93
doy <doy@tozt.net>
2009-02-12 03:46:40 -0500

    ItemPool doesn't need to wrap items it creates anymore

--- lib/TAEB/World/ItemPool.pm
+++ lib/TAEB/World/ItemPool.pm
@@ -4,26 +4,6 @@ extends 'NetHack::ItemPool';
 
 use constant inventory_class => 'TAEB::World::Inventory';
 
-around _create_item => sub {
-    my $orig = shift;
-    my $self = shift;
-
-    my $nhi = $self->$orig(@_);
-
-    return TAEB::World::Item->new_from_nhi($nhi);
-};
-
-after incorporate_artifact => sub {
-    my $self = shift;
-    my $item = shift;
-
-    my $pool_artifact = $self->artifacts->{ $item->artifact };
-    if (!$pool_artifact->isa('TAEB::World::Item')) {
-        $self->artifacts->{ $item->artifact } =
-            TAEB::World::Item->new_from_nhi($pool_artifact);
-    }
-};
-
 __PACKAGE__->meta->make_immutable;
 no TAEB::OO;
 
