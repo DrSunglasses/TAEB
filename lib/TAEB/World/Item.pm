31fdcca5657663cac7b5ee7efa23b757135d6311
bagofholding <bagofholding@gmail.com>
2009-02-11 19:06:29 -0500

    Fix bug causing enchantment not to show in debug_line

--- lib/TAEB/World/Item.pm
+++ lib/TAEB/World/Item.pm
@@ -61,7 +61,7 @@ sub debug_line {
         }
     }
 
-    if ($self->does('NetHack::Item::Enchantable')) {
+    if ($self->does('NetHack::Item:::Role::Enchantable')) {
         push @fields, $self->enchantment if $self->numeric_enchantment;
     }
 
