780d57818b24541e5d37c6384f457f595b6e3a33
doy <doy@tozt.net>
2009-02-12 00:24:39 -0500

    factor out creation of the appropriate TAEB::World::Item from a given NetHack::Item

--- lib/TAEB/World/ItemPool.pm
+++ lib/TAEB/World/ItemPool.pm
@@ -10,14 +10,7 @@ around _create_item => sub {
 
     my $nhi = $self->$orig(@_);
 
-    my $nhi_subclass = $nhi->meta->name;
-
-    # The right way to do this would be to search the TAEB::World::Item
-    # hierarchy for the class whose nhi attribute "isa" the $nhi_subclass.
-    # But this works fine. and I'm a bad person. ~ sartak
-    (my $taeb_class = $nhi_subclass) =~ s/^NetHack/TAEB::World/;
-
-    return $taeb_class->new(nhi => $nhi);
+    return TAEB::World::Item->new_from_nhi($nhi);
 };
 
 __PACKAGE__->meta->make_immutable;
