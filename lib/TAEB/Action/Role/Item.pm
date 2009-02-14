0ca718f7e055fa034fa932ce9567b0eacfe2494d
doy <doy@tozt.net>
2009-02-13 22:09:43 -0500

    trying to do an action with an item we don't have should be a warning, not just a debug message

--- lib/TAEB/Action/Role/Item.pm
+++ lib/TAEB/Action/Role/Item.pm
@@ -11,7 +11,8 @@ sub exception_missing_item {
     my $self = shift;
     return unless blessed $self->item;
 
-    TAEB->log->action("We don't have item " . $self->item . ", escaping.");
+    TAEB->log->action("We don't have item " . $self->item . ", escaping.",
+                      level => 'warning');
     TAEB->inventory->remove($self->item->slot);
     TAEB->enqueue_message(check => 'inventory');
     $self->aborted(1);
