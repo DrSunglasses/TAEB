4e2d05b848cdead69a16c8cebd5ea344e7ff98ff
doy <doy@tozt.net>
2009-02-17 03:34:11 -0500

    and... get rid of the default is => 'rw'

--- lib/TAEB/Meta/Attribute.pm
+++ lib/TAEB/Meta/Attribute.pm
@@ -2,24 +2,11 @@ package TAEB::Meta::Attribute;
 use Moose;
 extends 'Moose::Meta::Attribute';
 
-has '+is' => (
-    default => 'rw',
-);
-
 has 'provided' => (
     is      => 'rw',
     isa     => 'Bool',
     default => 0,
 );
-
-around _process_options => sub {
-    my $orig = shift;
-    my ($class, $name, $options) = @_;
-
-    $options->{is} ||= 'rw';
-
-    $orig->(@_);
-};
 
 sub clone_and_inherit_options {
     my ($self, %options) = @_;
