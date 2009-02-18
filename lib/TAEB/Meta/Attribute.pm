21f131c75133aa396042d83fd7dc46be7d099887
doy <doy@tozt.net>
2009-02-17 23:41:35 -0500

    this was an incredibly nasty hack

--- lib/TAEB/Meta/Attribute.pm
+++ lib/TAEB/Meta/Attribute.pm
@@ -8,28 +8,11 @@ has 'provided' => (
     default => 0,
 );
 
-sub clone_and_inherit_options {
-    my ($self, %options) = @_;
-
-    if ($options{isa}) {
-        my $type_constraint;
-        if (blessed($options{isa}) && $options{isa}->isa('Moose::Meta::TypeConstraint')) {
-            $type_constraint = $options{isa};
-        }
-        else {
-            $type_constraint = Moose::Util::TypeConstraints::find_or_create_type_constraint(
-                $options{isa}
-            );
-            (defined $type_constraint)
-                || confess "Could not find the type constraint '" . $options{isa} . "'";
-        }
-        $options{type_constraint} = $type_constraint;
-        delete $options{isa};
-    }
-
-
-    $self->clone(%options);
-}
+around legal_options_for_inheritance => sub {
+    my $orig = shift;
+    my $self = shift;
+    return ('provided', $self->$orig(@_));
+};
 
 no TAEB::OO;
 
