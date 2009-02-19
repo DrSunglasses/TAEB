cfc6853ef3656cf9103c5d1d26d8400505ab019c
doy <doy@tozt.net>
2009-02-19 01:26:16 -0500

    move the Initialize and Subscription roles from before make_immutable on the class metaclass into base class metaroles, and remove the class metaclass altogether. don't forget to unhackify this when moose 0.71 is released.

--- lib/TAEB/OO.pm
+++ lib/TAEB/OO.pm
@@ -4,20 +4,56 @@ use MooseX::ClassAttribute ();
 use Moose::Exporter;
 use Moose::Util::MetaRole;
 
-use TAEB::Meta::Class;
 use TAEB::Meta::Trait::Persistent;
 use TAEB::Meta::Trait::GoodStatus;
 use TAEB::Meta::Types;
 use TAEB::Meta::Overload;
 
+# XXX: replace the ugly gross stuff below here with this commented out version
+# once Moose 0.71 is released
+#Moose::Exporter->setup_import_methods(
+    #also        => ['Moose', 'MooseX::ClassAttribute'],
+    #with_caller => ['extends'],
+#);
+for my $sub (qw(with has before after around override augment super inner)) {
+    no strict 'refs';
+    *{$sub} = sub { goto \&{"Moose::$sub"} };
+}
 Moose::Exporter->setup_import_methods(
-    also => ['Moose', 'MooseX::ClassAttribute'],
+    also        => ['MooseX::ClassAttribute'],
+    with_caller => [
+        qw(extends with has before after around override augment),
+    ],
+    as_is => [
+        qw(super inner),
+        \&Carp::confess,
+        \&Scalar::Util::blessed,
+    ],
 );
+
+# make sure using extends doesn't wipe out our base class roles
+sub extends {
+    my ($caller, @superclasses) = @_;
+    Class::MOP::load_class($_) for @superclasses;
+    for my $parent (@superclasses) {
+        goto \&Moose::extends if $parent->can('does')
+                              && $parent->does('TAEB::Role::Initialize');
+    }
+    # i'm assuming that after apply_base_class_roles, we'll have a single
+    # base class...
+    my ($superclass_from_metarole) = $caller->meta->superclasses;
+    push @_, $superclass_from_metarole;
+    goto \&Moose::extends;
+}
 
 sub init_meta {
     shift;
     my %options = @_;
-    Moose->init_meta(%options, metaclass => 'TAEB::Meta::Class');
+    Moose->init_meta(%options);
+    Moose::Util::MetaRole::apply_base_class_roles(
+        for_class => $options{for_class},
+        roles     => ['TAEB::Role::Initialize', 'TAEB::Role::Subscription'],
+    );
     Moose::Util::MetaRole::apply_metaclass_roles(
         for_class                 => $options{for_class},
         attribute_metaclass_roles => ['TAEB::Meta::Role::Provided'],
--- lib/TAEB/Role/Subscription.pm
+++ lib/TAEB/Role/Subscription.pm
@@ -1,8 +1,14 @@
 package TAEB::Role::Subscription;
 use Moose::Role;
+use List::MoreUtils qw/any/;
 
 requires 'initialize';
-before initialize => sub { TAEB->publisher->subscribe(shift) };
+before initialize => sub {
+    my $self = shift;
+    TAEB->publisher->subscribe($self)
+        if (any { /^(?:msg|exception|respond)_/ || $_ eq 'send_message' }
+            $self->meta->get_method_list);
+};
 
 no Moose::Role;
 
