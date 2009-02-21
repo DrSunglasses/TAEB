91cfdc7a3ddff20b898ed119fac675b42a2b9062
Shawn M Moore <sartak@gmail.com>
2009-02-21 06:49:12 -0500

    Regex syntax errors will return all results

--- lib/TAEB/Display/Menu.pm
+++ lib/TAEB/Display/Menu.pm
@@ -99,7 +99,8 @@ around _item_metadata => sub {
     $search = "(?i:$search)" unless $search =~ /[A-Z]/;
 
     # compile regex
-    $search = qr/$search/;
+    $search = eval { qr/$search/ }
+        or return $orig->($self);
 
     return [ grep { $_->[0] =~ $search } @{ $orig->($self) } ];
 };
