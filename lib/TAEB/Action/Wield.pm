2fa4054f98c43917e8307d023e877b0722c0eb01
Stefan O'Rear <stefanor@cox.net>
2009-02-21 02:26:00 -0500

    Implement wield => item => 'nothing'

--- lib/TAEB/Action/Wield.pm
+++ lib/TAEB/Action/Wield.pm
@@ -6,15 +6,26 @@ with 'TAEB::Action::Role::Item' => { items => [qw/weapon/] };
 use constant command => "w";
 
 has '+weapon' => (
+    isa      => 'NetHack::Item | Str',
     required => 1,
 );
 
-sub respond_wield_what { shift->weapon->slot }
+sub respond_wield_what {
+    my $self = shift;
+
+    if (blessed $self->weapon) {
+        return $self->weapon->slot;
+    } elsif ($self->weapon eq "nothing") {
+        return "-";
+    } else {
+        TAEB->log->action("Unable to wield " . $self->weapon ".  Sending escape");
+        return "\e";
+    }
+}
 
 sub done {
     my $self = shift;
-    $self->weapon->is_wielded(1);
-
+    TAEB->inventory->wielded($self->weapon);
     # XXX: we need to track TAEB's offhand weapon too
 }
 
