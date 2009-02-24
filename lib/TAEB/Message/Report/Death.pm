6607e6cd11703147ef9a1bd3c7f3cfa8090c2bb9
Shawn M Moore <sartak@gmail.com>
2009-02-24 05:08:28 -0500

    death_report as_string

--- lib/TAEB/Message/Report/Death.pm
+++ lib/TAEB/Message/Report/Death.pm
@@ -3,15 +3,23 @@ use TAEB::OO;
 extends 'TAEB::Message::Report';
 
 has conducts => (
-    metaclass => 'Collection::Array',
-    is        => 'ro',
-    isa       => 'ArrayRef',
-    lazy      => 1,
-    default   => sub { [] },
-    provides  => {
+    metaclass  => 'Collection::Array',
+    is         => 'ro',
+    isa        => 'ArrayRef',
+    lazy       => 1,
+    default    => sub { [] },
+    auto_deref => 1,
+    provides   => {
         push => 'add_conduct',
     },
 );
+
+sub as_string {
+    my $self = shift;
+    my $conducts = join ', ', $self->conducts;
+
+    $conducts;
+}
 
 __PACKAGE__->meta->make_immutable;
 no TAEB::OO;
