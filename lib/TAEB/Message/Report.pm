bcc580e25b8332f48cb576f62bcd820af82be71a
Shawn M Moore <sartak@gmail.com>
2009-02-24 05:08:23 -0500

    Reports can be stringified

--- lib/TAEB/Message/Report.pm
+++ lib/TAEB/Message/Report.pm
@@ -2,6 +2,10 @@ package TAEB::Message::Report;
 use TAEB::OO;
 extends 'TAEB::Message';
 
+use overload (
+    q{""}    => 'as_string',
+    fallback => 1,
+);
 
 __PACKAGE__->meta->make_immutable;
 no TAEB::OO;
