ba721576724dae1357e1a268ba5ca64671be4274
Stefan O'Rear <stefanor@cox.net>
2009-02-23 21:30:55 -0500

    allow the AI to pick ring fingers

--- lib/TAEB/Action/Wear.pm
+++ lib/TAEB/Action/Wear.pm
@@ -7,6 +7,13 @@ has '+item' => (
     required => 1,
 );
 
+# Only matters if we have a choice in the matter (i.e. rings)
+has 'slot' => (
+    isa     => 'Str',
+    is      => 'ro',
+    default => 'left_hand',
+);
+
 sub command {
     my $self = shift;
     my $item = $self->item;
@@ -17,11 +24,20 @@ sub command {
 
 sub respond_wear_what { shift->item->slot }
 
-# arbitrary.. we don't need anything more advanced here
-sub respond_which_finger { 'l' }
+# yes, it makes the AI simpler if we know what slot we're putting stuff in
+sub respond_which_finger {
+    my $self = shift;
+
+    $self->slot eq 'right_ring' ? 'r' : 'l';
+}
 
 sub done {
     my $self = shift;
+
+    if ($self->item->type eq 'ring') {
+        $self->item->hand($self->slot eq 'right_hand' ? 'right' : 'left');
+    }
+
     $self->item->is_worn(1);
 }
 
