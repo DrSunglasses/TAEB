500ecd7b73bb1014f2c847d4e5e4571d99bd088d
Shawn M Moore <sartak@gmail.com>
2009-02-27 07:52:50 -0500

    Clean up TAEB::Debug::Console a bit

--- lib/TAEB/Debug/Console.pm
+++ lib/TAEB/Debug/Console.pm
@@ -7,38 +7,32 @@ sub msg_key {
     my $key = shift;
     return unless $key eq '~';
 
+    # Term::ReadLine seems to fall over on $ENV{PERL_RL} = undef?
+    $ENV{PERL_RL} ||= $self->config->{readline}
+        if $self->config && exists $self->config->{readline};
+
+    TAEB->display->deinitialize;
+
+    print "\n"
+        . "\e[1;37m+"
+        . "\e[1;30m" . ('-' x 50)
+        . "\e[1;37m[ "
+        . "\e[1;36mT\e[0;36mAEB \e[1;36mC\e[0;36monsole"
+        . " \e[1;37m]"
+        . "\e[1;30m" . ('-' x 12)
+        . "\e[1;37m+"
+        . "\e[m\n";
+
+    no warnings 'redefine';
+    require Devel::REPL::Script;
+
     eval {
+        local $SIG{__WARN__};
         local $SIG{__DIE__};
-
-        # Term::ReadLine seems to fall over on $ENV{PERL_RL} = undef?
-        $ENV{PERL_RL} ||= $self->config->{readline}
-            if $self->config && exists $self->config->{readline};
-
-        TAEB->display->deinitialize;
-
-        print "\n"
-            . "\e[1;37m+"
-            . "\e[1;30m" . ('-' x 50)
-            . "\e[1;37m[ "
-            . "\e[1;36mT\e[0;36mAEB \e[1;36mC\e[0;36monsole"
-            . " \e[1;37m]"
-            . "\e[1;30m" . ('-' x 12)
-            . "\e[1;37m+"
-            . "\e[m\n";
-
-        no warnings 'redefine';
-        require Devel::REPL::Script;
-
-        eval {
-            local $SIG{__WARN__};
-            local $SIG{__DIE__};
-            local $SIG{INT} = sub { die "Interrupted." };
-            Devel::REPL::Script->new->run;
-        };
+        local $SIG{INT} = sub { die "Interrupted." };
+        Devel::REPL::Script->new->run;
     };
 
-    # we really do need to do this twice. my amateur opinion is that curses
-    # isn't fully re-initialized when we call it the first time. oh well.
     TAEB->display->reinitialize;
 }
 
