0a6d639d92e80d66dc7fea898f24a232655d46f3
Stefan O'Rear <stefanor@cox.net>
2009-03-05 20:37:17 -0500

    Refactor ~ into a function

--- lib/TAEB/Debug/Console.pm
+++ lib/TAEB/Debug/Console.pm
@@ -7,6 +7,12 @@ sub msg_key {
     my $key = shift;
     return unless $key eq '~';
 
+    $self->repl();
+}
+
+sub repl {
+    my $self = shift;
+
     # Term::ReadLine seems to fall over on $ENV{PERL_RL} = undef?
     $ENV{PERL_RL} ||= $self->config->{readline}
         if $self->config && exists $self->config->{readline};
@@ -24,13 +30,26 @@ sub msg_key {
         . "\e[m\n";
 
     no warnings 'redefine';
-    require Devel::REPL::Script;
+    # using require doesn't call import, so no die handler is installed
+    eval {
+        local $SIG{__DIE__};
+        require Carp::REPL;
+    };
+
+    if ($@ && @_) {
+        # We're dropping into the REPL because of an error from somewhere,
+        # but Carp::REPL doesn't load (not installed?).  Report the actual
+        # error.
+        die @_;
+    } elsif ($@) {
+        die $@;
+    }
 
     eval {
         local $SIG{__WARN__};
         local $SIG{__DIE__};
         local $SIG{INT} = sub { die "Interrupted." };
-        Devel::REPL::Script->new->run;
+        Carp::REPL::repl(@_);
     };
 
     TAEB->display->reinitialize;
