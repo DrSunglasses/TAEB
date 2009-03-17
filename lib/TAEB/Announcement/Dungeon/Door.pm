d7aae305eba7b8eb9f1b1b34b9708ed133920e8c
Shawn M Moore <sartak@gmail.com>
2009-03-16 20:17:08 -0500

    parse door messages

--- lib/TAEB/Announcement/Dungeon/Door.pm
+++ lib/TAEB/Announcement/Dungeon/Door.pm
@@ -2,6 +2,36 @@ package TAEB::Announcement::Dungeon::Door;
 use TAEB::OO;
 extends 'TAEB::Announcement::Dungeon';
 
+__PACKAGE__->parse_messages(
+    "This door is locked." => {
+        state => 'locked',
+    },
+    "This door resists!" => {
+        state => 'resists',
+    },
+    "The door resists!" => {
+        state => 'resists',
+    },
+    "WHAMMM!!!" => {
+        state => 'resists',
+    },
+    "You succeed in unlocking the door." => {
+        state => 'just_unlocked',
+    },
+    "You succeed in picking the lock." => {
+        state => 'just_unlocked',
+    },
+    "You stop locking the door." => {
+        state => 'interrupted_locking',
+    },
+    "You stop picking the lock." => {
+        state => 'interrupted_unlocking',
+    },
+    "You stop unlocking the door." => {
+        state => 'interrupted_unlocking',
+    },
+);
+
 has state => (
     is       => 'ro',
     isa      => 'Str', # more general than DoorState
