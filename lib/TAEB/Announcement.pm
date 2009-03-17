8367995cd81f16200790ac5ef11eb7add93f9f5c
Shawn M Moore <sartak@gmail.com>
2009-03-16 19:41:42 -0500

    get/set for name_to_class

--- lib/TAEB/Announcement.pm
+++ lib/TAEB/Announcement.pm
@@ -19,6 +19,30 @@ sub name {
     return lc $class;
 }
 
+do {
+    my %name_to_class;
+
+    sub set_name_to_class {
+        my $self = shift;
+        my $name = shift;
+        my $class = shift;
+
+        if (exists $name_to_class{$name}) {
+            confess "Two announcement classes conflict over the same name '$name': $class and $name_to_class{$name}";
+        }
+
+        $name_to_class{$name} = $class;
+    }
+
+    sub name_to_class {
+        my $self = shift;
+        my $name = shift;
+
+        return $name_to_class{$name} if exists $name_to_class{$name};
+        confess "No announcement class with the name '$name' exists.";
+    }
+};
+
 __PACKAGE__->meta->make_immutable;
 no TAEB::OO;
 
