ea4cd0af62161aff560bbc66bffaedddbf5fe85e
Shawn M Moore <sartak@gmail.com>
2009-05-27 19:31:31 -0500

    Add an affect_type which gives us a target_tile for dungeon feature announcements

--- lib/TAEB/Announcement/Dungeon/Feature.pm
+++ lib/TAEB/Announcement/Dungeon/Feature.pm
@@ -1,9 +1,15 @@
 package TAEB::Announcement::Dungeon::Feature;
 use MooseX::Role::Parameterized;
+use Moose::Util::TypeConstraints 'enum';
 
 parameter tile_type => (
     is  => 'ro',
     isa => 'TAEB::Type::Tile',
+);
+
+parameter affect_type => (
+    is  => 'ro',
+    isa => (enum ['local', 'direction']),
 );
 
 role {
@@ -11,6 +17,13 @@ role {
     my $tile_type = $p->tile_type;
 
     method tile_type => sub { $tile_type };
+
+    if ($p->affect_type eq 'local') {
+        method target_tile => sub { TAEB->current_tile };
+    }
+    elsif ($p->affect_type eq 'direction') {
+        method target_tile => sub { TAEB->action->target_tile };
+    }
 };
 
 no MooseX::Role::Parameterized;
