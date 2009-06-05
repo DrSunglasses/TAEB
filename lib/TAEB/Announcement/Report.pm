1dea83216c26b2064bc289cdd25a7371972e85c8
Shawn M Moore <sartak@gmail.com>
2009-06-05 00:13:07 -0500

    Report->profile

--- lib/TAEB/Announcement/Report.pm
+++ lib/TAEB/Announcement/Report.pm
@@ -7,6 +7,12 @@ use overload (
     fallback => 1,
 );
 
+sub profile {
+    my @profile = TAEB->debug->profiler->analyze;
+    return "Profile:\n"
+         . join '', map { sprintf('%20s %.2g', $_->[0], $_->[2]) } @profile;
+}
+
 __PACKAGE__->meta->make_immutable;
 no TAEB::OO;
 
