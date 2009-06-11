37a8f0e7ca6271967ab8587b1738371370249b0b
Alex Smith <ais523@dell.linuxdev.us.dell.com>
2009-06-11 23:30:12 +0100

    An AI for testing endgame handling after an error

---
+++ lib/TAEB/AI/ErrorOut.pm
@@ -0,0 +1,19 @@
+package TAEB::AI::ErrorOut;
+use TAEB::OO;
+extends 'TAEB::AI';
+
+=head1 NAME
+
+TAEB::AI::ErrorOut - An AI that can't possibly work, for testing
+
+=cut
+
+# When asked for an action, throw an error to see how well the rest of
+# the framework handles it.
+sub next_action { die 'This error is totally unexpected.' }
+
+__PACKAGE__->meta->make_immutable;
+no TAEB::OO;
+
+1;
+
