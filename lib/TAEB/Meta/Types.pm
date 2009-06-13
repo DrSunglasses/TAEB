f498f707efcca7979c08ab875ee790cc08e3eab5
Shawn M Moore <sartak@gmail.com>
2009-06-13 19:55:57 -0400

    Get rid of the only duck_type I've seen in the wild

--- lib/TAEB/Meta/Types.pm
+++ lib/TAEB/Meta/Types.pm
@@ -28,11 +28,6 @@ enum 'TAEB::Type::Menu' => qw(none single multi);
 
 enum 'TAEB::Type::DeathState' => qw(inventory attributes kills conducts summary scores);
 
-# Ugh; is there a way to specify 'read or recv'? Unfortunately, we don't have
-# Perl6 junctions yet... As it is, the requirement for read/recv is replaced
-# by a die in TAEB::Interface::Local, but that isn't ideal.
-duck_type 'TAEB::Type::Pty' => qw(spawn write is_active);
-
 1;
 
 __END__
